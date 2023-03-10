e7176189ed9ee1a281693f7bf1374faab32a9485
李铭昕 <715557344@qq.com>
2023-03-10 10:08:59 +0800

    Build constructor of message.

--- php/src/ROCGenerator.php
+++ php/src/ROCGenerator.php
@@ -13,12 +13,38 @@ namespace Hyperf\ROCGenerator;
 
 use Google\Protobuf\Compiler\CodeGeneratorRequest;
 use Google\Protobuf\Compiler\CodeGeneratorResponse;
+use Google\Protobuf\DescriptorProto;
+use Google\Protobuf\FieldDescriptorProto;
 use Google\Protobuf\FileDescriptorProto;
+use PhpParser\Comment\Doc;
+use PhpParser\Lexer\Emulative;
+use PhpParser\Node;
+use PhpParser\Parser;
+use PhpParser\ParserFactory;
+use PhpParser\PrettyPrinter\Standard;
+use PhpParser\PrettyPrinterAbstract;
 use Throwable;
 
 class ROCGenerator
 {
     private Stream $stream;
+
+    private Parser $parser;
+
+    private PrettyPrinterAbstract $printer;
+
+    public function __construct()
+    {
+        $lexer = new Emulative([
+            'usedAttributes' => [
+                'comments',
+                'startLine', 'endLine',
+                'startTokenPos', 'endTokenPos',
+            ],
+        ]);
+        $this->parser = (new ParserFactory())->create(ParserFactory::ONLY_PHP7, $lexer);
+        $this->printer = new Standard();
+    }
 
     public function setStream(Stream $stream): static
     {
@@ -31,19 +57,14 @@ class ROCGenerator
         try {
             $request = new CodeGeneratorRequest();
             $request->mergeFromString((string) $this->stream);
-            $file = $request->getProtoFile()->getIterator();
+            $files = [];
             /** @var FileDescriptorProto $value */
-            foreach ($file as $value);
-            // var_dump($value->getOptions()->getPhpNamespace());
+            foreach ($request->getProtoFile()->getIterator() as $value) {
+                $files = array_merge($files, $this->generate($value));
+            }
 
             $response = new CodeGeneratorResponse();
-            $file = new CodeGeneratorResponse\File([
-                'name' => 'Test.php',
-                'content' => '<?php',
-            ]);
-            $response->setFile([
-                $file,
-            ]);
+            $response->setFile($files);
 
             fwrite(STDOUT, $response->serializeToString());
         } catch (Throwable $throwable) {
@@ -51,5 +72,112 @@ class ROCGenerator
         }
 
         return 0;
+    }
+
+    public function generate(FileDescriptorProto $proto): array
+    {
+        $result = [];
+        $namespace = $proto->getOptions()->getPhpNamespace() ?: $proto->getPackage() ?: 'RPC';
+        // Build Messages
+        /** @var DescriptorProto $message */
+        foreach ($proto->getMessageType()->getIterator() as $message) {
+            $result[] = $this->buildMessageCode($message, $namespace);
+        }
+
+        return $result;
+    }
+
+    protected function buildMessageCode(DescriptorProto $message, string $namespace): CodeGeneratorResponse\File
+    {
+        $namespace = $this->buildNamespace($namespace);
+        $class = $this->buildClass($message);
+
+        $namespace->stmts[] = $class;
+
+        $content = $this->printer->prettyPrint([
+            $this->buildDeclare(),
+            $namespace,
+        ]);
+
+        return new CodeGeneratorResponse\File([
+            'name' => $message->getName() . '.php',
+            'content' => '<?php' . PHP_EOL . $content,
+        ]);
+    }
+
+    protected function buildDeclare(): Node\Stmt\Declare_
+    {
+        return new Node\Stmt\Declare_([
+            new Node\Stmt\DeclareDeclare(new Node\Identifier('strict_types'), new Node\Scalar\LNumber(1)),
+        ]);
+    }
+
+    protected function buildNamespace(string $namespace): Node\Stmt\Namespace_
+    {
+        return new Node\Stmt\Namespace_(
+            new Node\Name($namespace),
+            [
+                $this->buildUse(),
+            ],
+            [
+                'comments' => [$this->buildComment()],
+            ]
+        );
+    }
+
+    protected function buildClass(DescriptorProto $message): Node\Stmt\Class_
+    {
+        $params = [];
+        /** @var FieldDescriptorProto $field */
+        foreach ($message->getField()->getIterator() as $field) {
+            $params[] = new Node\Param(
+                new Node\Expr\Variable($field->getName()),
+                null,
+                new Node\Identifier($this->toPHPType($field->getType())),
+                flags: Node\Stmt\Class_::MODIFIER_PUBLIC
+            );
+        }
+
+        return new Node\Stmt\Class_($message->getName(), [
+            'implements' => [
+                new Node\Name('JsonSerializable'),
+            ],
+            'stmts' => [
+                new Node\Stmt\ClassMethod(
+                    new Node\Identifier('__construct'),
+                    [
+                        'flags' => Node\Stmt\Class_::MODIFIER_PUBLIC,
+                        'params' => $params,
+                    ]
+                ),
+            ],
+        ]);
+    }
+
+    protected function buildUse(): Node\Stmt\Use_
+    {
+        return new Node\Stmt\Use_([
+            new Node\Stmt\UseUse(
+                new Node\Name('JsonSerializable')
+            ),
+        ]);
+    }
+
+    protected function buildComment(): Doc
+    {
+        return new Doc('/**
+ * Generated by the ROC Generator.  DO NOT EDIT!
+ */');
+    }
+
+    protected function toPHPType(int $type): string
+    {
+        return match ($type) {
+            FieldDescriptorProto\Type::TYPE_BOOL => 'bool',
+            FieldDescriptorProto\Type::TYPE_INT32, FieldDescriptorProto\Type::TYPE_INT64 => 'int',
+            FieldDescriptorProto\Type::TYPE_STRING => 'string',
+            FieldDescriptorProto\Type::TYPE_MESSAGE => 'object',
+            default => 'mixed',
+        };
     }
 }
