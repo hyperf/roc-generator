7b084f2b5a589f996df8879f4560ddcacad95503
李铭昕 <715557344@qq.com>
2023-03-12 09:04:02 +0800

    Build linux and windows.

--- .github/workflows/fetch_micro_artifacts_id.php
+++ .github/workflows/fetch_micro_artifacts_id.php
@@ -2,6 +2,10 @@
 $arch = [
     'macos.arm' => '592209492',
     'macos.x86_64' => '592209493',
+    'linux.musl.aarch64' => '593883918',
+    'linux.musl.x86_64' => '593883919',
+    'linux.glibc.x86_64' => '593883910',
+    'windows.x86_64' => '593809854'
 ];
 $id = $arch[getenv('ARCH')];
 echo "::set-output name=id::" . $id . PHP_EOL;
