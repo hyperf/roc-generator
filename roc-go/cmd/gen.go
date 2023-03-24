427d94ef8b1defe37b42a9d0b83c911b72306d95
李铭昕 <715557344@qq.com>
2023-03-24 17:11:16 +0800

    Added executor of protoc.

--- roc-go/cmd/gen.go
+++ roc-go/cmd/gen.go
@@ -1,17 +1,31 @@
 package cmd
 
 import (
-	"fmt"
 	"github.com/spf13/cobra"
+	"os"
+	"os/exec"
 )
 
 var genCmd = &cobra.Command{
-	Use:   "gen:roc",
+	Use:   "gen:roc [file to print]",
 	Short: "Generate protoc command",
 	Long:  `Generate protoc command which used to generate structs and interfaces by protobuf.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(1234)
+		bin, err := os.Executable()
+		if err != nil {
+			panic(err)
+		}
+
+		executor := exec.Command(
+			"protoc",
+			"--plugin=protoc-gen-roc="+bin,
+			"--proto_path=.",
+			"--roc_out=.",
+			"example.proto",
+		)
+
+		executor.Run()
 	},
 }
